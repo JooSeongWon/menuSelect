2f3302e7aa1e55f1cc40d8034becab5c5f3ebef1
JooSeongWon <scan0359@naver.com>
2023-04-23 23:43:04 +0900

    init

---
+++ choice-backend/src/test/java/com/simpleMenu/choice/ChoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleMenu.choice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
